0fd3bcbf218c0834df985c13789f3cc28aee5119
xulin <xl@gameduchy.com>
2023-10-07 16:40:44 +0800

    Single Shader 2D Renderer render both texture

--- XLEngine/src/XLEngine/Renderer/Texture.cpp
+++ XLEngine/src/XLEngine/Renderer/Texture.cpp
@@ -6,6 +6,20 @@
 
 namespace XLEngine
 {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			XL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+		XL_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -14,7 +28,7 @@ namespace XLEngine
 			XL_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 		XL_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
