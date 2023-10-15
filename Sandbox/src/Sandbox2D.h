d48932d8bf8e9f4888a2343e072e94d00a012c61
XuLin <1522517184@qq.com>
2023-10-15 10:56:01 +0800

    SpriteSheets

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -24,7 +24,8 @@ private:
 	XLEngine::Ref<XLEngine::Shader> m_FlatColorShader;
 
 	XLEngine::Ref<XLEngine::Texture2D> m_CheckerboardTexture;
-
+	XLEngine::Ref<XLEngine::Texture2D> m_SpriteSheet;
+	
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f,0.8f,1.0f };
 
 	ParticleProps m_Particle;
