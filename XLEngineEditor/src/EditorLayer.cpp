3980ac3631b588f9c2c77e3190f91610103472a4
xulin <xl@gameduchy.com>
2023-12-20 16:24:20 +0800

    Play Button

--- XLEngineEditor/src/EditorLayer.cpp
+++ XLEngineEditor/src/EditorLayer.cpp
@@ -22,6 +22,8 @@ namespace XLEngine
     void EditorLayer::OnAttach()
     {
         m_CheckerboardTexture = Texture2D::Create("assets/textures/Checkerboard.png");
+        m_IconPlay = Texture2D::Create("Resources/Icons/PlayButton.png");
+        m_IconStop = Texture2D::Create("Resources/Icons/StopButton.png");
 
         FramebufferSpecification fbSpec;
         fbSpec.Attachments = { 
@@ -112,13 +114,6 @@ namespace XLEngine
         }
 
         m_Framebuffer->ClearAttachment(1, -1);
-
-        // Update
-        if (m_ViewportFocused)
-        {
-            if (m_ViewportFocused)
-                m_CameraController.OnUpdate(ts);
-        }
         
         m_EditorCamera.OnUpdate(ts);
 
@@ -161,9 +156,26 @@ namespace XLEngine
             m_Framebuffer->Unbind();
         }*/
 
+        switch (m_SceneState)
+        {
+        case XLEngine::EditorLayer::SceneState::Edit:
+        {
+            if (m_ViewportFocused)
+                m_CameraController.OnUpdate(ts);
 
-        // Update scene
-        m_ActiveScene->OnUpdateEditor(ts, m_EditorCamera);
+            m_EditorCamera.OnUpdate(ts);
+
+            m_ActiveScene->OnUpdateEditor(ts, m_EditorCamera);
+            break;
+        }
+        case XLEngine::EditorLayer::SceneState::Play:
+        {
+            m_ActiveScene->OnUpdateRuntime(ts);
+            break;
+        }
+        default:
+            break;
+        }
 
         auto [mx, my] = ImGui::GetMousePos();
         mx -= m_ViewportBounds[0].x;
@@ -374,6 +386,36 @@ namespace XLEngine
         ImGui::End();
         ImGui::PopStyleVar();
 
+        UI_Toolbar();
+
+        ImGui::End();
+    }
+
+    void EditorLayer::UI_Toolbar()
+    {
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 2));
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemInnerSpacing, ImVec2(0, 0));
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0, 0, 0, 0));
+        auto& colors = ImGui::GetStyle().Colors;
+        const auto& buttonHovered = colors[ImGuiCol_ButtonHovered];
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(buttonHovered.x, buttonHovered.y, buttonHovered.z, 0.5f));
+        const auto& buttonActive = colors[ImGuiCol_ButtonActive];
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(buttonActive.x, buttonActive.y, buttonActive.z, 0.5f));
+
+        ImGui::Begin("##toolbar", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse);
+
+        float size = ImGui::GetWindowHeight() - 4.0f;
+        Ref<Texture2D> icon = m_SceneState == SceneState::Edit ? m_IconPlay : m_IconStop;
+        ImGui::SetCursorPosX((ImGui::GetWindowContentRegionMax().x * 0.5f) - (size * 0.5f));
+        if (ImGui::ImageButton((ImTextureID)icon->GetRendererID(), ImVec2(size, size), ImVec2(0, 0), ImVec2(1, 1), 0))
+        {
+            if (m_SceneState == SceneState::Edit)
+                OnScenePlay();
+            else if (m_SceneState == SceneState::Play)
+                OnSceneStop();
+        }
+        ImGui::PopStyleVar(2);
+        ImGui::PopStyleColor(3);
         ImGui::End();
     }
 
@@ -474,6 +516,16 @@ namespace XLEngine
             SceneSerializer serializer(m_ActiveScene);
             serializer.Serialize(filepath);
         }
+    }
+
+    void EditorLayer::OnScenePlay()
+    {
+        m_SceneState = SceneState::Play;
+    }
+
+    void EditorLayer::OnSceneStop()
+    {
+        m_SceneState = SceneState::Edit;
     }
 
 }
--- XLEngineEditor/src/EditorLayer.h
+++ XLEngineEditor/src/EditorLayer.h
@@ -29,6 +29,12 @@ namespace XLEngine
 		void OpenScene();
 		void OpenScene(const std::filesystem::path& path);
 		void SaveSceneAs();
+
+		void OnScenePlay();
+		void OnSceneStop();
+
+		// UI Panels
+		void UI_Toolbar();
 	private:
 		OrthographicCameraController m_CameraController;
 
@@ -59,8 +65,18 @@ namespace XLEngine
 
 		int m_GizmoType = -1;
 
+		enum class SceneState
+		{
+			Edit = 0,
+			Play = 1
+		};
+		SceneState m_SceneState = SceneState::Edit;
+
 		// Panels
 		SceneHierarchyPanel m_SceneHierarchyPanel;
 		ContentBrowserPanel m_ContentBrowserPanel;
+
+		// Editor resources
+		Ref<Texture2D> m_IconPlay, m_IconStop;
 	};
 }
